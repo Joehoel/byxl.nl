2c0401b1b806fb24322ccfaf310b168027054345
Joël Kuijper <jmkuijper@outlook.com>
2023-08-05 16:40:10 +0200

    feat: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<svelte:head>
+	<title>BYXL</title>
+</svelte:head>
+
+<main class="grid text-center place-content-center bg-stone-950 text-gray-100 h-screen">
+	<slot />
+</main>
